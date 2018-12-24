a5c36d4c52f66c10a8b7435067a6cca065025ac9
Verity Hill <verity.lowenna@gmail.com>
2018-12-24 20:57:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj;
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
 }
