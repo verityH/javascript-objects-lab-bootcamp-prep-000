93418b8b6bfe0edd148e30b892ac4f60e5c2eae4
Verity Hill <verity.lowenna@gmail.com>
2018-12-24 20:53:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-function deleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
-}
+
 
