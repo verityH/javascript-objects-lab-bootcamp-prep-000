762e812bdcbb089c3f4460931a318f6b88b9a231
Verity Hill <verity.lowenna@gmail.com>
2018-12-24 20:48:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = { foo: 'bar' }
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value });
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
